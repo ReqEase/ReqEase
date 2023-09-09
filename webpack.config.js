17088a2fb0d96131253ad2883deec6026fc31819
HichemTab <konanhichemsinshi@gmail.com>
2023-09-09 23:44:05 +0100

    workflows actions

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require("webpack");
 // noinspection JSUnresolvedReference
 let bannerPlugin = new webpack.BannerPlugin({
     banner: `/*!
- * ReqEase v1.0.8
+ * ReqEase v1.0.9
  * (c) HichemTech
  * Released under the MIT License.
  * Github: github.com/ReqEase/ReqEase
